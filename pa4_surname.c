2edc22d8b713509e045a7ff3ecf04983b4fef693
arielfevry <125998099+arielfevry@users.noreply.github.com>
2025-04-08 03:01:37 +0000

    commit changes

--- pa4_surname.c
+++ pa4_surname.c
@@ -14,8 +14,6 @@
 // function prototypes defined here
 unsigned long get_the_element(unsigned long *, unsigned long);
 
-
-
 // DO NOT MODIFY THIS PORTION OF THE MAIN FUNCTION
 int main(void) {
     FILE *input_file = fopen(INPUT_FILE_NAME, "r");
@@ -57,7 +55,75 @@ int main(void) {
 }
 // DO NOT MODIFY THIS PORTION OF THE MAIN FUNCTION
 
+// define BSTNode and Tree structures
+typedef struct BSTNode_s {
+    int data;
+    struct BSTNode_s *left;
+    struct BSTNode_s *right;
+} BSTNode;
 
+typedef struct BSTree_s {
+    BSTNode *root;
+} BSTree;
+
+BSTNode* createNode(unsigned long value){
+    //malloc node
+    BSTNode *newNode = malloc(sizeof(BSTNode));
+
+    //set the data and left/right values of the new Node;
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+BSTNode* insertNode(BSTNode *root, unsigned long value){
+
+    //base case if tree is empty (root == NULL)
+    if(root == NULL){
+        return createNode(value); //will be the node inserted
+    }
+
+    //tree isnt empty
+    if(value <= root->data){ // if value is less than or equal go left on the binary tree
+        root->left = insertNode(root->left, value); //call insert node again new root = left
+    }
+    else if(value > root->data){ // greater than go right
+        root->right = insertNode(root->right, value); 
+    }
+
+    return root; //working way back up binary tree returning correct roots 
+}
+
+void inorderTraversal(BSTNode *root, unsigned long *arr, int* index){
+
+    // base case reached end of tree or tree is empty
+    if (root != NULL) {
+
+        // traverse LVR left first to get smallest nodes
+        inorderTraversal(root->left, arr, index);
+
+        // store nodes value in array at current index
+        arr[*index] = root->data;
+        (*index)++; // update index ++
+
+        // R of inorder LVR trvaerse right to get larger nodes after smaller
+        inorderTraversal(root->right, arr, index);
+    }
+}
+
+void freeTree(BSTNode *root){
+    //if there node exists / havent reach off of the end of the tree
+    if(root != NULL){
+        //traverse left and right until end if reached
+        freeTree(root->left);
+        freeTree(root->right);
+
+        //free the node back up the recurisve calls
+        free(root);
+    }
+}
 
 unsigned long get_the_element(unsigned long *arr, unsigned long size) {
     // TODO START
@@ -65,11 +131,29 @@ unsigned long get_the_element(unsigned long *arr, unsigned long size) {
     // of an odd-length list. Assume that all the numbers
     // in the list are distinct.
 
+    //malloc and intialize tree
+    BSTree *tree = malloc(sizeof(BSTree));
+    tree->root = NULL;
+    
+    // for each element in array
+    for(int i = 0; i < size; i++){
+        tree->root = insertNode(tree->root, arr[i]); //insert each element into the tree;
+    }
 
+    // new array consisting of sorted values
+    unsigned long *sortedArr = malloc(sizeof(unsigned long) * size);
+    int index = 0;
 
+    // populate array with sorted valye by preforming in order binary tree traversal
+    inorderTraversal(tree->root, sortedArr, &index);
 
-
-
+    //save middle element in array and free sorted and tree
+    int midElement = sortedArr[size / 2];
+    free(sortedArr);
+    freeTree(tree->root);
+    free(tree);
+    
+    return midElement; //return the middle index of the sorted array
     // TODO END
 }
 
